df0d2d5773b600620321fb87d1692a419638cf37
Michele Long <michelelongcode@gmail.com>
2018-01-10 14:57:02 -0600

    variable scope change

--- shitshow.js
+++ shitshow.js
@@ -1,8 +1,10 @@
-var shitshow;
-var goesWell = "That's great! Share your good fortune!";
-var goesBoom = "I'm sorry! I hope tomorrow is better!";
+let shitshow;
 
 function check_day() {
+
+    let goesWell = "That's great! Share your good fortune!";
+    let goesBoom = "I'm sorry! I hope tomorrow is better!";
+
     if (shitshow == true) {
         document.getElementById("result").innerHTML = goesBoom;
     } else if (shitshow == false) {
