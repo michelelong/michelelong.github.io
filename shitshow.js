1b08034c8fea99d9e41cdb30277c860d53531993
Michele Long <michelelongcode@gmail.com>
2018-01-23 15:56:01 -0600

    Add files via upload
    charged to let variable, eventlisteners and queryselectors

--- shitshow.js
+++ shitshow.js
@@ -1,13 +1,9 @@
-let shitshow;
+let shitshow = document.querySelector(".boxb");
+let rainbows = document.querySelector(".boxc");
 
-function check_day() {
-
-    let goesWell = "That's great! Share your good fortune!";
-    let goesBoom = "I'm sorry! I hope tomorrow is better!";
-
-    if (shitshow == true) {
-        document.getElementById("result").innerHTML = goesBoom;
-    } else if (shitshow == false) {
-        document.getElementById("result").innerHTML = goesWell;
-    }
-}
+shitshow.addEventListener("click", function(){
+	document.getElementById("result").innerHTML = "I'm sorry! I hope tomorrow is better!";
+	});
+rainbows.addEventListener("click", function(){
+	document.getElementById("result").innerHTML = "That's great! Share your good fortune!";
+	});
