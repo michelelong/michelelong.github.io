fc35502518c1fb269af4125a7f36c9e5db0aac0a
Michele Long <michelelongcode@gmail.com>
2018-03-02 11:25:56 -0600

    className fix

--- shitshow.js
+++ shitshow.js
@@ -5,9 +5,9 @@ const hideShit = document.getElementById("boxb");
 const headline = document.getElementsByClassName("headline")[0];
 let howBadIsIt = 0;
 
-shitshow.addEventListener("click", function() {
+shitshow.addEventListener("click", function () {
   headline.textContent = "How bad was your day?";
-  hideRainbow.style.display = "none";
+  hideRainbow.className = "hidden";
   howBadIsIt -= 1;
   if (howBadIsIt === -1) {
     headline.textContent = "Hm, that doesn't seem too bad.";
@@ -20,15 +20,14 @@ shitshow.addEventListener("click", function() {
   }
   if (howBadIsIt === -4) {
     headline.textContent = "Oh, shit! Watch where you step!";
-    hideShit.style.display = "none";
     document.body.className = "yuck";
   }
 });
 
-rainbows.addEventListener("click", function() {
+rainbows.addEventListener("click", function () {
   howBadIsIt += 1;
   headline.textContent = "How nice was your day?";
-  hideShit.style.display = "none";
+  hideShit.className = "hidden";
   if (howBadIsIt === 1) {
     headline.textContent = "A decent day, but nothing special.";
   }
@@ -41,6 +40,5 @@ rainbows.addEventListener("click", function() {
   if (howBadIsIt === 4) {
     document.body.className = "spew";
     headline.textContent = "Your happiness is contagious!";
-    hideRainbow.style.display = "none";
   }
 });
